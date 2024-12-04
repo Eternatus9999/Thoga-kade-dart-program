3013650edcd62e3c401a71815810e6015fbbb9ba
Eternatus9999 <eternatus9999@gmail.com>
2024-12-04 21:57:21 +0530

    Add Headings to the Main menu and sub Menus

---
+++ lib/models/Vegitable.dart
@@ -0,0 +1,10 @@
+class Vegetable {
+  final String id;
+  final String name;
+  final double pricePerKg;
+  final double availableQuantity;
+  final String category;
+  final DateTime expiryDate;
+
+  Vegetable(this.id, this.name, this.pricePerKg, this.availableQuantity, this.category, this.expiryDate);
+}
